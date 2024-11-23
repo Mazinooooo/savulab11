7de70965d4bbab2bb03048624ee03e85e9fd5522
Mazinooooo <146594261+Mazinooooo@users.noreply.github.com>
2024-11-23 13:35:38 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'provider.dart';
 
 void main() {
   runApp(
@@ -29,67 +29,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RadiusProvider with ChangeNotifier {
-  double _topLeftRadius = 0;
-  double _topRightRadius = 0;
-  double _bottomLeftRadius = 0;
-  double _bottomRightRadius = 0;
-
-  double get topLeftRadius => _topLeftRadius;
-  double get topRightRadius => _topRightRadius;
-  double get bottomLeftRadius => _bottomLeftRadius;
-  double get bottomRightRadius => _bottomRightRadius;
-
-  void setTopLeftRadius(double newRadius) {
-    _topLeftRadius = newRadius;
-    notifyListeners();
-  }
-
-  void setTopRightRadius(double newRadius) {
-    _topRightRadius = newRadius;
-    notifyListeners();
-  }
-
-  void setBottomLeftRadius(double newRadius) {
-    _bottomLeftRadius = newRadius;
-    notifyListeners();
-  }
-
-  void setBottomRightRadius(double newRadius) {
-    _bottomRightRadius = newRadius;
-    notifyListeners();
-  }
-}
-
-
-class RoundedContainer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final radiusProvider = Provider.of<RadiusProvider>(context);
-
-    return Container(
-      width: 150,
-      height: 150,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(radiusProvider.topLeftRadius),
-          topRight: Radius.circular(radiusProvider.topRightRadius),
-          bottomLeft: Radius.circular(radiusProvider.bottomLeftRadius),
-          bottomRight: Radius.circular(radiusProvider.bottomRightRadius),
-        ),
-      ),
-    );
-  }
-}
-
 class RadiusConfigurationSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        RoundedContainer(),
+        RoundedContainer(), 
         SizedBox(height: 20),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -132,39 +78,24 @@ class RadiusConfigurationSection extends StatelessWidget {
     );
   }
 }
-class RadiusSlider extends StatelessWidget {
-  final String label;
-  final double Function(RadiusProvider) getValue;
-  final void Function(RadiusProvider, double) setValue;
 
-  RadiusSlider({
-    required this.label,
-    required this.getValue,
-    required this.setValue,
-  });
-
+class RoundedContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<RadiusProvider>(
-      builder: (context, radiusProvider, child) {
-        double value = getValue(radiusProvider);
+    final radiusProvider = Provider.of<RadiusProvider>(context);
 
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('$label: ${value.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            Slider(
-              min: 0,
-              max: 75,
-              value: value,
-              onChanged: (newRadius) => setValue(radiusProvider, newRadius),
-              activeColor: Colors.deepPurple,
-              inactiveColor: Colors.deepPurple[100],
-            ),
-          ],
-        );
-      },
+    return Container(
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(radiusProvider.topLeftRadius),
+          topRight: Radius.circular(radiusProvider.topRightRadius),
+          bottomLeft: Radius.circular(radiusProvider.bottomLeftRadius),
+          bottomRight: Radius.circular(radiusProvider.bottomRightRadius),
+        ),
+      ),
     );
   }
 }
